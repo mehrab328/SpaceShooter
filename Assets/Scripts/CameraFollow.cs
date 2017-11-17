4c81e1ad68983c4c1d47a4dc7264437211be0c48
mehrab328 <mehrab.zaman@outlook.com>
2017-11-18 00:00:21 +0600

    First Commit

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform myTarget;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(myTarget != null){
+			Vector3 targPos = myTarget.position;
+			targPos.z = transform.position.z;
+			transform.position = targPos;
+		}
+	}
+}
